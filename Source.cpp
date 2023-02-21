908a88acda5224ea908034ea00de3359681d664f
Denis Obozintsev <obozincevdenis@gmail.com>
2023-02-21 22:03:37 +0300

    Добавлен обработчик ошибки умножения матриц

--- Source.cpp
+++ Source.cpp
@@ -121,6 +121,7 @@ double** Matrix::operator-(Matrix& obj) {
 double** Matrix::operator*(Matrix& obj) {
 	if (width != obj.height) {
 		cout << "Умножение не возможно" << endl;
+		throw -1;
 	}
 	else {
 		double** arr = new double* [height];
@@ -157,37 +158,43 @@ double** Matrix::operator*(double x)
 }
 
 int main() {
-	setlocale(0, "ru");
-	int n, m;
-	cin >> n >> m;
-	double** arr = new double*[n];
-	for (int i = 0; i < n; i++) {
-		arr[i] = new double[m];
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+		setlocale(0, "ru");
+	try {
+		int n, m;
+		cin >> n >> m;
+		double** arr = new double* [n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new double[m];
 		}
-	}
-	Matrix matrix1(n, m, arr);
-	for (int i = 0; i < n; i++) {
-		delete[]arr[i];
-	}
-	delete[]arr;
-	int n3 = n;
-	cin >> n >> m;
-	int m3 = m;
-	arr = new double* [n];
-	for (int i = 0; i < n; i++) {
-		arr[i] = new double[m];
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cin >> arr[i][j];
+			}
 		}
+		Matrix matrix1(n, m, arr);
+		for (int i = 0; i < n; i++) {
+			delete[]arr[i];
+		}
+		delete[]arr;
+		int n3 = n;
+		cin >> n >> m;
+		int m3 = m;
+		arr = new double* [n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new double[m];
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cin >> arr[i][j];
+			}
+		}
+		Matrix matrix2(n, m, arr);
+		Matrix matrix3(n3, m3, matrix1 * matrix2);
+		matrix3.printMatrix();
+		return 0;
 	}
-	Matrix matrix2(n, m, arr);
-	Matrix matrix3(n3, m3, matrix1 * matrix2);
-	matrix3.printMatrix();
-	return 0;
+	catch (int) {
+		cout << "Произошла ошибка" << endl;
+		return 1;
+	}
 }
